ae6030f8453aff0205fec5aaf8ed0265fd4650d7
Calvin Tong <calvin.tongcy@gmail.com>
2015-08-23 00:45:10 +0800

    plot1.R is ready

---
+++ plot1.R
@@ -0,0 +1,30 @@
+## Set working directory
+setwd('D:/Dropbox/Private/BIG_Data/CourseWork/ExploratoryDataAnalysis/Project2')
+
+filename <- "NEI_data.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata/data/NEI_data.zip"
+  download.file(fileURL, filename)
+}  
+if (!file.exists("NEI_data")) { 
+  unzip(filename) 
+}
+
+if(!exists("NEI")){
+  NEI <- readRDS("./summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./Source_Classification_Code.rds")
+}
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
